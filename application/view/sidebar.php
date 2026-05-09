71f38003d4be488373ca714827bb36585ccaeac1
FauzanAmirudin <fauzan223360@gmail.com>
2026-05-09 22:02:42 +0700

    fix(sidebar): show monitor menu for operator role

--- application/view/sidebar.php
+++ application/view/sidebar.php
@@ -104,7 +104,7 @@ defined('GF_BASE_PATH') or exit('No direct script access allowed');
 					</li>
 				<?php } ?>
 
-				<?php if (is_level("Admin, Monitor")) { ?>
+				<?php if (is_level("Admin, Monitor, Operator")) { ?>
 					<li class="sidebar-item <?php echo ($current_ctrl == 'admin' && $current_func == 'monitor') ? 'active' : ''; ?>">
 						<a href="<?php echo set_url("admin/monitor"); ?>" title="Monitor">
 							<svg class="sidebar-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"><rect x="2" y="3" width="20" height="14" rx="2" ry="2"></rect><line x1="8" y1="21" x2="16" y2="21"></line><line x1="12" y1="17" x2="12" y2="21"></line></svg>
