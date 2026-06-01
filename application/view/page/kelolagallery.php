62908471035a185c7c5c3bcd1332fa92aff1e8fd
Fauzan Amirudin Basith <fauzan223360@gmail.com>
2026-06-01 08:43:12 +0700

    fix: resolve javascript syntax error in delete modal onclick handler caused by unescaped newlines using json_encode

--- application/view/page/kelolagallery.php
+++ application/view/page/kelolagallery.php
@@ -43,7 +43,7 @@ require_level('Admin, Operator');
                 <svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"><path d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7"></path><path d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z"></path></svg>
                 Edit
               </a>
-              <button type="button" onclick="openDeleteModal(<?php echo $photo['ID']; ?>, '<?php echo htmlspecialchars(addslashes(!empty($photo['KETERANGAN']) ? mb_strimwidth($photo['KETERANGAN'], 0, 30, '...') : 'foto ini'), ENT_QUOTES); ?>')" title="Hapus" class="btn-delete-sm">
+              <button type="button" onclick="openDeleteModal(<?php echo $photo['ID']; ?>, <?php echo htmlspecialchars(json_encode(!empty($photo['KETERANGAN']) ? mb_strimwidth($photo['KETERANGAN'], 0, 30, '...') : 'foto ini'), ENT_QUOTES, 'UTF-8'); ?>)" title="Hapus" class="btn-delete-sm">
                 <svg xmlns="http://www.w3.org/2000/svg" width="13" height="13" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"><polyline points="3 6 5 6 21 6"></polyline><path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"></path></svg>
               </button>
             </div>
