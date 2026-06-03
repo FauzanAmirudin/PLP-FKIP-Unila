0d421b5d52962c5110ca52e034aa2548f827ce7b
Fauzan Amirudin Basith <fauzan223360@gmail.com>
2026-06-03 09:28:11 +0700

    chore: remove timestamp from css versioning

--- application/config/config.php
+++ application/config/config.php
@@ -50,7 +50,7 @@ defined('GF_BASE_PATH') OR exit('No direct script access allowed');
 		<meta name="msapplication-TileImage" content="'. $config['base_url'] .'/assets/assets/images/ms-icon-144x144.png">
 	';
 	$config['site_css'] 			= Array(
-		'css/style.min.css?v='.time()
+		'css/style.min.css'
 	);
 	$config['site_js'] 					= Array(
 		'js/script.min.js'
